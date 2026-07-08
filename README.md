e20f3f6d258e6a1b12242051a4d3e22f617a8d60
Moonstruckk <1057920841@qq.com>
2026-07-08 23:31:43 +0800

    Initial commit with auto-deploy

--- README.md
+++
@@ -1,1 +0,0 @@
-# lyrportfolio.github.io
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/lyrportfolio.github.io/', // 关键配置：设置基础路径
-})
