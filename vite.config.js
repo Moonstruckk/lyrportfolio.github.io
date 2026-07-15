f06ba817db99995a13bee65f525fff2cf600e8f3
Moonstruckk <1057920841@qq.com>
2026-07-15 17:21:42 +0800

    auto-detect deployment platform for base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lyrportfolio.github.io/',
+  base: process.env.VERCEL ? '/' : '/lyrportfolio.github.io/',
 })
